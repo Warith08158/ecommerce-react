9767e97528def2415debf5a338e7b62f8998fb66
solaudeen <solaudeenw@gmail.com>
2024-05-01 05:13:24 -0700

    testing deployment

--- src/pages/home_page/utils/NewArrivalsGallery.jsx
+++ src/pages/home_page/utils/NewArrivalsGallery.jsx
@@ -22,7 +22,7 @@ const NewArrivalsGallery = () => {
             <div className="flex  w-[80%] p-4 sm:w-48 rounded-md shadow-lg flex-col gap-3">
               <div className="flex justify-center">
                 <img
-                  src={`/public/${product.image}`}
+                  src={`/${product.image}`}
                   alt={product.name}
                   className="object-contain max-h-44 sm:w-32 sm:h-32"
                 />
